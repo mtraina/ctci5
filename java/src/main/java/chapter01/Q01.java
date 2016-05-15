a14fc8a8cf4006158cf72f47d5f0fee977f454a1
mtraina <matteo.traina.mail@gmail.com>
2016-05-15 12:39:32 +0200

    is unique chars II

--- java/src/main/java/chapter01/Q01.java
+++ java/src/main/java/chapter01/Q01.java
@@ -4,11 +4,38 @@ import static java.util.Objects.isNull;
 
 public class Q01 {
 
+    // assumption: the text is (extended) ASCII encoded
     public static boolean isUniqueChars(String text){
         if(isNull(text) || text.length() > 256){
             return false;
         }
 
+        boolean[] chars = new boolean[256];
+
+        for(char c : text.toCharArray()) {
+            int i = (int) c;
+            if (chars[i]) {
+                return false;
+            } else {
+                chars[i] = true;
+            }
+        }
         return true;
+    }
+
+    public static boolean isUniqueChars2(String text){
+        if(isNull(text) || text.length() > 256){
+            return false;
+        }
+
+        boolean[] chars = new boolean[256];
+
+        return text.chars().boxed().map(i -> {
+            if(chars[i]) return false;
+            else {
+                chars[i] = true;
+                return true;
+            }
+        }).filter(b -> !b).findFirst().orElse(true);
     }
 }
--- java/src/test/groovy/chapter01/Q01Spec.groovy
+++ java/src/test/groovy/chapter01/Q01Spec.groovy
@@ -4,12 +4,23 @@ import spock.lang.Specification
 
 class Q01Spec extends Specification {
 
+    def "is unique chars should return false for a text longer then 256 chars"(){
+        given:
+        def text = ''
+        (0..256).forEach {text += (char)it}
+
+        expect:
+        !Q01.isUniqueChars2(text)
+    }
+
     def "is unique chars should return #expected when receives #text"() {
         expect:
-        Q01.isUniqueChars(text) == expected
+        Q01.isUniqueChars2(text) == expected
 
         where:
         text    | expected
         null    | false
+        "abc"   | true
+        "defghd"| false
     }
 }
